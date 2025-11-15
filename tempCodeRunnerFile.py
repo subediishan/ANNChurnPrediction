9285c67df608137b29d7c115fc6c871e4642c02e
Ishan Subedi <ishan20633@gmail.com>
2025-11-15 21:44:09 +0545

    floder cleaned

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-geo_encoded =onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded,columns=onehot_encoder_geo.get_feature_names_out(['Geography'])) 
-print("great")
-  
